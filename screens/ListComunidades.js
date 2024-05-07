991c09945a5f893844229d852afa4700764d4db1
albertoesco <alumno.538326@ies.azarquiel.es>
2024-05-07 16:35:10 +0200

    BanderasComunidades

--- screens/ListComunidades.js
+++ screens/ListComunidades.js
@@ -1,13 +1,16 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View, ScrollView, TouchableOpacity } from "react-native";
+import { StyleSheet, Text, View, ScrollView, TouchableOpacity, Image } from "react-native";
 import appFirebase from "../credenciales";
 import { getFirestore, collection, getDocs } from "firebase/firestore";
 import { useEffect, useState } from "react";
+import { getStorage, ref, getDownloadURL } from "firebase/storage";
 
 const db = getFirestore(appFirebase);
+const storage = getStorage(appFirebase);
 
 export default function ListComunidades(props) {
   const [lista, setLista] = useState([]);
+  const [imagenes, setImagenes] = useState({});
 
   useEffect(() => {
     const getLista = async () => {
@@ -29,6 +32,23 @@ export default function ListComunidades(props) {
     getLista();
   }, []);
 
+  useEffect(() => {
+    const obtenerImagenes = async () => {
+      const imagenes = {};
+      for (const comunidad of lista) {
+        const imagePath = `comunidades/${comunidad.nombre}/${comunidad.nombre}.jpg`; // Ruta de la imagen en el almacenamiento
+        try {
+          const url = await getDownloadURL(ref(storage, imagePath)); // Obtiene la URL de descarga de la imagen
+          imagenes[comunidad.idcomunidad] = url;
+        } catch (error) {
+          console.error(`Error al obtener la imagen de ${comunidad.nombre}:`, error);
+        }
+      }
+      setImagenes(imagenes);
+    };
+    obtenerImagenes();
+  }, [lista]);
+
   return (
     <ScrollView>
       <View style={styles.container}>
@@ -40,6 +60,11 @@ export default function ListComunidades(props) {
               props.navigation.navigate("Provincias", { nombreComunidad: list.nombre })
             }
           >
+            {/* Aquí se muestra la imagen de la comunidad */}
+            {imagenes[list.idcomunidad] && (
+              <Image source={{ uri: imagenes[list.idcomunidad] }} style={styles.flagImage} />
+            )}
+            {/* Aquí se muestra el nombre de la comunidad */}
             <Text style={styles.cardText}>{list.nombre}</Text>
           </TouchableOpacity>
         ))}
@@ -62,13 +87,19 @@ const styles = StyleSheet.create({
     padding: 20,
     marginVertical: 10,
     borderRadius: 10,
-    alignItems: "center",
-    justifyContent: "center",
+    flexDirection: "row", // Para alinear elementos horizontalmente
+    alignItems: "center", // Para centrar verticalmente los elementos
+    justifyContent: "flex-start", // Para alinear los elementos a la izquierda
     alignSelf: "center",
   },
   cardText: {
     fontSize: 18,
     fontWeight: "bold",
     textAlign: "center",
+  },
+  flagImage: {
+    width: 30, // Ajustar el ancho de la imagen según sea necesario
+    height: 20, // Ajustar la altura de la imagen según sea necesario
+    marginRight: 10, // Espacio entre la imagen de la bandera y el texto
   },
 });
