c12abde3752be732d4b5ea51d3ed57e17d0661f7
albertoesco <alumno.538326@ies.azarquiel.es>
2024-05-14 17:24:00 +0200

    Login&ChatFuncionan

--- screens/Chat.js
+++ screens/Chat.js
@@ -1,13 +1,32 @@
 import React, { useState, useEffect } from "react";
+import { View, Text, Button, StyleSheet } from "react-native";
 import { GiftedChat } from "react-native-gifted-chat";
-import { collection, addDoc, serverTimestamp, onSnapshot } from "firebase/firestore";
+import { collection, addDoc, serverTimestamp, onSnapshot, query, orderBy } from "firebase/firestore";
+import { getAuth, onAuthStateChanged } from "firebase/auth";
 import { db } from '../firebase/credenciales'; // Importa la instancia de Firebase Firestore
+import Modal from 'react-native-modal';
+import Icon from 'react-native-vector-icons/MaterialIcons';
 
 export default function Chat() {
   const [messages, setMessages] = useState([]);
+  const [user, setUser] = useState(null);
+  const [username, setUsername] = useState('');
+  const [isModalVisible, setModalVisible] = useState(false);
 
   useEffect(() => {
-    const unsubscribe = onSnapshot(collection(db, "mensajes"), (snapshot) => {
+    const auth = getAuth();
+    const unsubscribeAuth = onAuthStateChanged(auth, (currentUser) => {
+      if (currentUser) {
+        setUser(currentUser);
+        setUsername(currentUser.email.split('@')[0]);  // Obtener la parte antes del '@' del correo electrónico
+      } else {
+        setUser(null);
+        setUsername('');
+      }
+    });
+
+    const q = query(collection(db, "mensajes"), orderBy("fecha", "desc"));
+    const unsubscribeMessages = onSnapshot(q, (snapshot) => {
       const messagesData = snapshot.docs.map((doc) => {
         const data = doc.data();
         return {
@@ -22,16 +41,24 @@ export default function Chat() {
       });
       setMessages(messagesData);
     });
-  
-    return () => unsubscribe();
+
+    return () => {
+      unsubscribeAuth();
+      unsubscribeMessages();
+    };
   }, []);
 
   const onSend = async (newMessages = []) => {
+    if (!user) {
+      setModalVisible(true); // Muestra el modal de error
+      return;
+    }
     const message = newMessages[0];
     try {
       await addDoc(collection(db, "mensajes"), {
         texto: message.text,
-        usuario: message.user._id,
+        usuario: user.uid,
+        usuario_nombre: username,  // Agregar el nombre del usuario al mensaje
         fecha: serverTimestamp(), // Guarda la marca de tiempo actual
       });
     } catch (error) {
@@ -39,5 +66,34 @@ export default function Chat() {
     }
   };
 
-  return <GiftedChat messages={messages} onSend={onSend} user={{ _id: 1 }} />;
+  const closeModal = () => {
+    setModalVisible(false);
+  };
+
+  return (
+    <View style={{ flex: 1 }}>
+      <GiftedChat messages={messages} onSend={onSend} user={user ? { _id: user.uid, name: username } : null} />
+      <Modal isVisible={isModalVisible}>
+        <View style={styles.modalContent}>
+          <Icon name="error" size={50} color="red" />
+          <Text style={styles.modalText}>Debe iniciar sesión para enviar mensajes</Text>
+          <Button title="Cerrar" onPress={closeModal} />
+        </View>
+      </Modal>
+    </View>
+  );
 }
+
+const styles = StyleSheet.create({
+  modalContent: {
+    backgroundColor: 'white',
+    padding: 20,
+    borderRadius: 10,
+    alignItems: 'center',
+  },
+  modalText: {
+    fontSize: 18,
+    marginVertical: 10,
+    textAlign: 'center',
+  },
+});
--- screens/Login.js
+++ screens/Login.js
@@ -1,12 +1,20 @@
 import React, { useState } from 'react';
 import { View, Text, TextInput, Button, StyleSheet, Alert } from 'react-native';
-import { createUserWithEmailAndPassword } from 'firebase/auth';
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut } from 'firebase/auth';
 import { auth } from '../firebase/credenciales';
+import { useNavigation } from '@react-navigation/native';
+import Modal from 'react-native-modal';
+import Icon from 'react-native-vector-icons/MaterialIcons';
 
 export default function Login() {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [error, setError] = useState(null);
+  const [isModalVisible, setModalVisible] = useState(false);
+  const [modalMessage, setModalMessage] = useState('');
+  const [username, setUsername] = useState('');  // Nuevo estado para el nombre de usuario
+
+  const navigation = useNavigation();
 
   const handleEmailChange = (text) => {
     setEmail(text);
@@ -32,20 +40,74 @@ export default function Login() {
     return true;
   };
 
-  const handleSignUp = async () => {
+  const showModal = (message) => {
+    setModalMessage(message);
+    setModalVisible(true);
+  };
+
+  const hideModal = () => {
+    setModalVisible(false);
+  };
+
+  const handleSignInOrSignUp = async () => {
     if (!validateInput()) return;
     try {
-      const userCredential = await createUserWithEmailAndPassword(auth, email, password);
-      console.log('Usuario registrado con éxito:', userCredential.user.email);
-      // Lógica adicional después del registro exitoso, como navegar a otra pantalla
+      // Intentar iniciar sesión
+      const userCredential = await signInWithEmailAndPassword(auth, email, password);
+      console.log('Usuario iniciado sesión con éxito:', userCredential.user.email);
+      const user = email.split('@')[0];  // Obtener el nombre de usuario
+      setUsername(user);
+      showModal('Inició sesión con éxito');
+      setTimeout(() => {
+        hideModal();
+        navigation.navigate('Comunidades'); // Navega a la pantalla de la lista de comunidades
+      }, 1000); // Tiempo para mostrar el modal antes de navegar
+    } catch (signInError) {
+      if (signInError.code === 'auth/user-not-found') {
+        // Si el usuario no existe, intentar registrarlo
+        try {
+          const userCredential = await createUserWithEmailAndPassword(auth, email, password);
+          console.log('Usuario registrado con éxito:', userCredential.user.email);
+          const user = email.split('@')[0];  // Obtener el nombre de usuario
+          setUsername(user);
+          showModal('Registrado con éxito');
+          setTimeout(() => {
+            hideModal();
+            navigation.navigate('Comunidades'); // Navega a la pantalla de la lista de comunidades
+          }, 1000); // Tiempo para mostrar el modal antes de navegar
+        } catch (signUpError) {
+          setError(signUpError.message);
+          console.log('Error al registrar el usuario:', signUpError.message);
+        }
+      } else {
+        setError(signInError.message);
+        console.log('Error al iniciar sesión:', signInError.message);
+      }
+    }
+  };
+
+  const handleSignOut = async () => {
+    try {
+      await signOut(auth);
+      console.log('Usuario cerró sesión con éxito');
+      setUsername('');  // Limpiar el nombre de usuario al cerrar sesión
+      showModal('Cerró sesión con éxito');
+      setTimeout(() => {
+        hideModal();
+        navigation.navigate('Login'); // Navega a la pantalla de inicio de sesión
+      }, 1000); // Tiempo para mostrar el modal antes de navegar
     } catch (error) {
-      setError(error.message);
-      console.log('Error al registrar el usuario:', error.message);
+      setError('Error al cerrar sesión');
+      console.log('Error al cerrar sesión:', error);
     }
   };
 
   return (
     <View style={styles.container}>
+      <View style={styles.userContainer}>
+        <Text style={styles.userLabel}>Usuario:</Text>
+        <Text style={styles.username}>{username}</Text>
+      </View>
       <Text style={styles.title}>Inicio Sesión / Registro</Text>
       <TextInput
         style={styles.input}
@@ -63,7 +125,16 @@ export default function Login() {
       {error && (
         <Text style={styles.error}>{error}</Text>
       )}
-      <Button title="Registrarse" onPress={handleSignUp} />
+      <View style={styles.buttonContainer}>
+        <Button title="Iniciar Sesión / Registrarse" onPress={handleSignInOrSignUp} />
+        <Button title="Cerrar Sesión" onPress={handleSignOut} />
+      </View>
+      <Modal isVisible={isModalVisible}>
+        <View style={styles.modalContent}>
+          <Icon name="check-circle" size={50} color="green" />
+          <Text style={styles.modalText}>{modalMessage}</Text>
+        </View>
+      </Modal>
     </View>
   );
 }
@@ -74,6 +145,21 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
     padding: 20,
+  },
+  userContainer: {
+    position: 'absolute',
+    top: 40,
+    right: 20,
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  userLabel: {
+    fontSize: 16,
+    fontWeight: 'bold',
+  },
+  username: {
+    fontSize: 16,
+    marginLeft: 5,
   },
   title: {
     fontSize: 24,
@@ -88,8 +174,24 @@ const styles = StyleSheet.create({
     marginBottom: 10,
     paddingHorizontal: 10,
   },
+  buttonContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    width: '100%',
+  },
   error: {
     color: 'red',
     marginBottom: 10,
+  },
+  modalContent: {
+    backgroundColor: 'white',
+    padding: 20,
+    borderRadius: 10,
+    alignItems: 'center',
+  },
+  modalText: {
+    fontSize: 18,
+    marginVertical: 10,
+    textAlign: 'center',
   },
 });
